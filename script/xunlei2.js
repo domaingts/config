0c4b9fbb7e727a3e63d20b89d0bd38b31c3eca13
domaingts <139693268+domaingts@users.noreply.github.com>
2026-03-27 14:41:21 +0000

    update

---
+++ script/xunlei2.js
@@ -0,0 +1,18 @@
+export default async function (ctx) {
+    const obj = await ctx.response?.json() ?? {};
+    obj.vipList = [{
+        "expireDate": "20290609",
+        "isAutoDeduct": "0",
+        "isVip": "1",
+        "isYear": "1",
+        "payId": "0",
+        "payName": "---",
+        "register": "0",
+        "vasid": "2",
+        "vasType": "5",
+        "vipDayGrow": "20",
+        "vipGrow": "840",
+        "vipLevel": "7"
+    }];
+    return { body: obj }
+}
